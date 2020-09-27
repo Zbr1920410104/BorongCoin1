f20f35943bbc541ea4b0afc7f10cfe91369cd39b
Zbr1920410104 <765451419@qq.com>
2020-09-27 11:52:59 +0800

    完成上传

--- bilibiliDemo/main.js
+++ bilibiliDemo/main.js
@@ -1,10 +1,10 @@
 const ecLib = require('elliptic').ec;
 const ec = new ecLib('secp256k1') // curve name
 // 如果作为依赖安装，请用这种方式import
-// const { Transaction, Chain, Block } = require("luotuocoin");
+// const { Transaction, Chain, Block } = require("borongcoin");
 const { Transaction, Chain, Block } = require("./blockChainDemo");
 
-const luotuoCoin = new Chain(3);
+const borongCoin = new Chain(3);
 const keyPairSender = ec.genKeyPair();
 const privateKeySender = keyPairSender.getPrivate('hex')
 const publicKeySender = keyPairSender.getPublic('hex')
@@ -18,11 +18,11 @@ t1.sign(ec.keyFromPrivate(privateKeySender))
 console.log(t1)
 // t1.amount=20
 // const t2 = new Transaction("addr2", "addr1", 5);
-luotuoCoin.addTransaction(t1);
-// luotuoCoin.addTransaction(t2);
+borongCoin.addTransaction(t1);
+// borongCoin.addTransaction(t2);
 
-// console.log(luotuoCoin)
-luotuoCoin.mineTransactionPool("addr3");
-console.log(luotuoCoin.validateChain())
-console.log(luotuoCoin)
-console.log(luotuoCoin.chain[1])
+// console.log(borongCoin)
+borongCoin.mineTransactionPool("addr3");
+console.log(borongCoin.validateChain())
+console.log(borongCoin)
+console.log(borongCoin.chain[1])
--- bilibiliDemo/proofOfWork.js
+++ bilibiliDemo/proofOfWork.js
@@ -1,7 +1,7 @@
 const sha256 = require("crypto-js/sha256");
 
-console.log(sha256("luotuo1").toString());
-console.log(sha256("luotuo2").toString());
+console.log(sha256("borong1").toString());
+console.log(sha256("borong2").toString());
 
 // 对于不同的输入，哪怕只是一个很微小的改动，输出的结果都是很不一样的。
 // 对于同一个输入，输出结果是一样的。
@@ -10,7 +10,7 @@ console.log(sha256("luotuo2").toString());
 // 我现在需要得到 一个 开头值前4位全为0的哈希值。请告诉我X是多少
 
 function proofOfWork() {
-  let data = "luotuo";
+  let data = "borong";
   let x = 1;
   while (true) {
     if (
